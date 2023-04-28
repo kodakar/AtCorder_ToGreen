ac5b3fa48106dba8d8705272acc19f951eee97f4
kodakar <kodakarar@gmail.com>
2023-04-28 23:45:10 +0900

    bit

---
+++ bit全探索/C - Switches.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+
+k = []
+
+for i in range(N):
+    a = list(map(int, input().split()))
+    k.append(a[1:])
+
+p = list(map(int, input().split()))
+
+for i in range(1 << N):
+    for shift in (N):
+        pass
+
+print(N, M)
+print(k)
+print(p)
