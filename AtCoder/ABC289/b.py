f27e89c3b509837b6eb7cea0e042b03156c3bf07
kodakar <kodakarar@gmail.com>
2023-05-28 10:24:48 +0900

    ABC289 B - レ

--- AtCoder/ABC289/b.py
+++ AtCoder/ABC289/b.py
@@ -1,16 +1,17 @@
-N, M = map(int, input().split())
-a = list(map(int, input().split()))
+from collections import deque
 
-p = []
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+ans = []
+tmp = deque()
 
 for i in range(1, N + 1):
-    p.append(i)
+    if i not in A:
+        tmp.appendleft(i)
+        ans += list(tmp)
+        tmp.clear()
+    else:
+        tmp.appendleft(i)
 
-for i in range(M):
-    x, y = a[i] - 1, a[i]
-    if a[i] + 1 == a[i + 1]:
-        pass
-    
-    p[x : y + 1] = p[y : x]
-
-print(p)
+print(*ans)
