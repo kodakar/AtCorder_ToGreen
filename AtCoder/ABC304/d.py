5b1368067f31a50f01e4dfd7c798cf614abb03f1
kodakar <kodakarar@gmail.com>
2023-06-03 22:07:31 +0900

    ABC304 A Piece of Cake Not complete

---
+++ AtCoder/ABC304/d.py
@@ -0,0 +1,10 @@
+W, H = map(int, input().split())
+
+N = int(input())
+pq = [list(map(int, input().split())) for _ in range(N)]
+
+A = int(input())
+a = [list(map(int, input().split()))]
+
+B = int(input())
+b = [list(map(int, input().split()))]
