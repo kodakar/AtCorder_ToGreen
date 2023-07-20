2a34ce232f54999acf908f3cec1e6cd11be4c45f
kodakar <kodakarar@gmail.com>
2023-07-20 22:57:11 +0900

    ABC309

--- AtCoder/ABC309/c.py
+++ AtCoder/ABC309/c.py
@@ -1,20 +1,20 @@
 N, K = map(int, input().split())
 
-a = [list(map(int, input().split())) for _ in range(N)]
+A = [list(map(int, input().split())) for _ in range(N)]
 
-a.sort()
+A.sort()
 
 sum = 0
 
-for i in a:
+for i in A:
     sum += i[1]
 
 if sum <= K:
     print(1)
     exit()
 
-for i in range(len(a)):
+for a, b in A:
+    sum -= b
     if sum <= K:
-        print(a[i-1][0]+1)
+        print(a+1)
         exit()
-    sum -= a[i][1]
