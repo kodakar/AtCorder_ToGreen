ed4ac51867de716c855ec9c8141e6ea1830b11a3
kodakar <kodakarar@gmail.com>
2023-07-21 19:39:13 +0900

    ABC308

--- AtCoder/ABC308/c.py
+++ AtCoder/ABC308/c.py
@@ -4,7 +4,7 @@ a = []
 
 for i in range(N):
     A, B = map(int, input().split())
-    a.append([(A/(A+B)), -i-1])         # -i - 1 にする事で、確率が同じ時に昇順になる
+    a.append([(10**19*A//(A+B)), -i-1])         # -i - 1 にする事で、確率が同じ時に昇順になる 10**19*A//(A+B) で不動小数点対策
 
 ans = []
 for i in sorted(a, reverse=True):
